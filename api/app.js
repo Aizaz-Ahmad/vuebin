363349782cc5c8c72154ac78a44bcd955bb7b23b
Aizaz Ahmad <aizazahamd90@gmail.com>
2021-09-12 18:18:15 +0500

    Update Environmental Vars

--- api/app.js
+++ api/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: process.env.CLIENT_URL,
   })
 );
 app.use('/api', require('./router'));
--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
 const app = require('./app');
 const MongoClient = require('mongodb').MongoClient;
 const DAO = require('./dao');
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 console.log(process.env.CLIENT_URL, process.env.MONGO_DB_URL);
 
