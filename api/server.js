a0b2f9b0f6edc3662f788d357bc786df6e099095
Aizaz Ahmad <aizazahamd90@gmail.com>
2021-09-12 16:58:18 +0500

    Update server.js to Test for Production

--- api/server.js
+++ api/server.js
@@ -3,7 +3,10 @@ const MongoClient = require('mongodb').MongoClient;
 const DAO = require('./dao');
 require('dotenv').config();
 
+console.log(process.env);
+
 const PORT = process.env.PORT || 3000;
+
 MongoClient.connect(process.env.MONGO_DB_URL).then(async client => {
   DAO.initCollection(client.db().collection('codes'));
   app.listen(PORT, () => console.log('listening on port ' + PORT));
